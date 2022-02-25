ecb94cb90f29457557236591292676dc51270fba
frsiam <siam.frs@gmail.com>
2022-02-25 15:16:26 +0600

    modified

--- js/comments.js
+++ js/comments.js
@@ -6,13 +6,13 @@ const loadComments = () => {
 // loadComments()
 
 const displayComments = data => {
-    console.log(data)
+    // console.log(data)
     data.forEach(comment => {
         console.log(comment)
         const commentDiv = document.getElementById('comments')
         const div = document.createElement('div')
-        div.classList.add('container','border', 'border-2', 'bg-info','my-2', 'py-3')
-        div.innerHTML =`
+        div.classList.add('container', 'border', 'border-2', 'bg-info', 'my-2', 'py-3')
+        div.innerHTML = `
         <div onclick="commentsDetails('${comment}')">
             <h5>Name: ${comment.name}</h5>
             <h6>Email: ${comment.email}</h6>
@@ -24,7 +24,4 @@ const displayComments = data => {
 
 const commentsDetails = post => {
     console.log(post)
-    /* fetch('https://jsonplaceholder.typicode.com/comments')
-        .then(res => res.json())
-        .then(data => console.log(data[0].id)) */
 }
